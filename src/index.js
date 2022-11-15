9e6f5f33729812a979b451e002560ede5aad45c7
M4RTiT0 <martotamborini8@gmail.com>
2022-11-15 17:18:43 -0300

    fgh

--- src/index.js
+++ src/index.js
@@ -1,28 +1,73 @@
 const express = require('express')
 const cors = require('cors')
-const socketio = require('socket.io') 
+const morgan = require('morgan')
+const socketio = require('socket.io')
+const osu = require('node-os-utils')
 
+/* eslint-disable */
+//aca obtengo toda la funcionalidad de express, depositada en la constante serve
 const server = express()
 
-server.set('port', process.env.PORT || 3000)
+//especificamos el formato de datos que va a manipular nuestor servidor, es decir, nuestra API
+server.use(express.json())
 
+//nos va a permitir comunicar al cliente con el servidor, y viceversa
 server.use(cors())
 
-server.listen(server.get('port'), () => {
-    console.log('la conexion fue exitosa!')
-});
+//nos va a notificar en la consola, cada vez que se haga petición HTTP(put,delete,post,get)
+server.use(morgan('dev'))
 
-//le pasamos el servidor a socketio para habilitar el canal websocket
+//seteamos puerto disponible en el sistema
+server.set('port', process.env.PORT || 3000)
+
+//poner las rutas en funcionamiento
+server.use(require('./routes/cliente.route'))
+
+
+//le damos arranque a nuestro servidor
+const servidor = server.listen( server.get('port') )
+
+console.log(`Servidor corriendo en el puerto: ${server.get('port')}`)
+
+
 const io = socketio(servidor)
+const cpu = osu.cpu
+const drive = osu.drive
+const memoria = osu.mem
+const netstat = osu.netstat
 
-//establecemos la apertura del canal para emitir posteriormente los datos
-io.on('connection', (socket) => {
-    setinterval( () => {
-        socket.emit('dato-socket', 10)
-    }, 1000)
+io.on('connection', () => {
 
-    socket.on('respuesta', (valor) => {
-        console.log(valor)
-    });
+  setInterval(() => {
+    cpu.usage().then((info)=>{
+      io.sockets.emit('uso_cpu', info )
+      console.log(info)
+    })
 
-});
+    cpu.free().then((info)=>{
+      io.sockets.emit('cpu_free', info)
+      console.log(info)
+    })
+
+    /*drive.info().then((freePercentage,totalGb)=>{
+      io.sockets.emit('El espacio libre que hay es: ', freePercentage, ' por ciento')
+      console.log(freePercentage)
+      io.sockets.emit('El total de las gigas que hay son: ', totalGb, ' GB')
+      console.log(totalGb)
+    })*/
+
+    memoria.free().then((totalMemMb)=>{
+      io.sockets.emit('memoria_free', totalMemMb.freeMemMb)
+      console.log(totalMemMb)
+    })
+
+    memoria.used().then((usedMemMb)=>{
+      io.sockets.emit('memoria_utilizar', usedMemMb , ' MB')
+      io.sockets.emit('memoria_utilizar', usedMemMb , ' MB')
+      console.log(usedMemMb)
+    })
+
+
+
+  },1000)
+})
